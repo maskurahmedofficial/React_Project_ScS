0a6e4bb9b107572ae0c24a8f213a27a5e60cc03d
Maskur Ahmed <maskurahmed37@gmail.com>
2025-10-22 21:46:50 +0600

    leading clear

--- src/components/Section1/LeftContant.jsx
+++ src/components/Section1/LeftContant.jsx
@@ -5,7 +5,7 @@ const LeftContant = () => {
     <>
       <div className="h-full w-1/3 flex flex-col justify-between ">
         <div className="p-6">
-          <h3 className=" leading-tight text-7xl font-bold mb-7">
+          <h3 className=" text-7xl font-bold mb-7">
             Prospective <br /> <span className="text-gray-200">Customer</span> <br /> Segmentation
           </h3>
           <p className="text-xl font-medium text-gray-400 mb-5 ">
