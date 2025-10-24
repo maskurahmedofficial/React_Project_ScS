314313ab150ec8ab7200190a613a010f8dee482f
Maskur Ahmed <maskurahmed37@gmail.com>
2025-10-24 16:08:20 +0600

    image added on card

---
+++ src/components/Section1/RightCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const RightCard = () => {
+  return (
+    <>
+    
+    <div className='h-full w-80 bg-red-400 rounded-2xl'>
+    <img src="https://images.unsplash.com/photo-1665436035665-d7dad9086ee2?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=759" alt="img" className='h-full w-fit object-center ' />
+    </div>
+
+
+    </>
+  )
+}
+
+export default RightCard
--- src/components/Section1/RightContent.jsx
+++ src/components/Section1/RightContent.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import RightCard from './RightCard'
 
 const RightContent = () => {
   return (
     <>
     
-    <div className='h-full w-2/3 bg-green-300'>
-
+    <div className='h-full w-2/3 bg-green-300 p-5'>
+      <RightCard/>
     </div>
 
     </>
