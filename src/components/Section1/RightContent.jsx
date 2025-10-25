8de06f4a3356da45a002b6134cbbb9922a8bd04d
Maskur Ahmed <maskurahmed37@gmail.com>
2025-10-25 23:47:34 +0600

    space added

--- src/components/Section1/RightContent.jsx
+++ src/components/Section1/RightContent.jsx
@@ -7,6 +7,7 @@ const RightContent = (props) => {
     <>
     
     <div id='right' className='h-full flex flex-nowrap gap-10 overflow-x-auto w-2/3 rounded-2xl p-5'>
+    
       {props.users.map((elem,idx)=> <RightCard key={idx} id={idx}  tag={elem.tag}  img={elem.image}
       
       /> )}
