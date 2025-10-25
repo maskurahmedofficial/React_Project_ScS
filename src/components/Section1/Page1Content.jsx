2571a521e9948a0fcc83218ca57e6b4d3f023687
Maskur Ahmed <maskurahmed37@gmail.com>
2025-10-25 22:17:50 +0600

    css changed

--- src/components/Section1/Page1Content.jsx
+++ src/components/Section1/Page1Content.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import LeftContant from './LeftContant'
 import RightContent from './RightContent'
 
-const Page1Content = () => {
+const Page1Content = (props) => {
   return (
     <>
     <div className='h-[90vh] bg-gray-600 p-5 gap-10 py-10 flex justify-between items-center'>
 
     <LeftContant/>
-    <RightContent/>
+    <RightContent users = {props.users} />
     
     </div>
     </>
--- src/components/Section1/RightContent.jsx
+++ src/components/Section1/RightContent.jsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import RightCard from './RightCard'
 
-const RightContent = () => {
+const RightContent = (props) => {
+  console.log(props.users)
   return (
     <>
     
-    <div className='h-full flex flex-nowrap overflow-x-auto gap-10 w-2/3 rounded-2xl bg-green-200 p-5'>
-      <RightCard/>
-      <RightCard/>
-      <RightCard/>
-      <RightCard/>
-      <RightCard/>
-      <RightCard/>
+    <div className='h-full flex flex-nowrap overflow-x-auto gap-10 w-2/3 rounded-2xl p-5'>
+      {props.users.map}
     </div>
 
     </>
