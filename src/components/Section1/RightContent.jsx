28384e4cf645e534887f8a52628055de875331e5
Maskur Ahmed <maskurahmed37@gmail.com>
2025-10-26 23:49:23 +0600

    two

--- src/components/Section1/RightContent.jsx
+++ src/components/Section1/RightContent.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import RightCard from './RightCard'
+import React from "react";
+import RightCard from "./RightCard";
 
 const RightContent = (props) => {
-  console.log(props.users)
+  console.log(props.users);
   return (
     <>
-    
-    <div id='right' className='h-full flex flex-nowrap gap-10 overflow-x-auto w-2/3 rounded-2xl p-5'>
-
-      {props.users.map((elem,idx)=> <RightCard key={idx} id={idx}  tag={elem.tag}  img={elem.image}
-      
-      /> )}
-    </div>
+      <div
+        id="right"
+        className="h-full flex flex-nowrap gap-10 overflow-x-auto w-2/3 rounded-2xl p-5"
+      >
+        {props.users.map((elem, idx) => (
+          <RightCard key={idx} id={idx} tag={elem.tag} img={elem.image} />
+        ))}
+        
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default RightContent
+export default RightContent;
